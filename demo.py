e5b2101537d10aea0b9c429aa751629e7240c4c4
LAMXkE <sametim17@gmail.com>
2023-06-20 21:25:03 +0900

    fix: first version that works

--- demo.py
+++ demo.py
@@ -12,24 +12,30 @@ def generate_carrier(frequency, duration, sampling_rate, vol):
 
 
 def modulate_data(binary_data, mark_frequency, space_frequency, sampling_rate, duration, vol):
-    carrier = generate_carrier(mark_frequency, duration, sampling_rate, vol)
-    modulated_signal = bytearray()
+    # carrier = generate_carrier(mark_frequency, duration, sampling_rate, vol)
+    modulated_signals = []
+    for packet in binary_data:
+        modulated_signal = np.array([])
+        for i, bit in enumerate(packet):
+            frequency = mark_frequency if bit == 1 else space_frequency
+            carrier = generate_carrier(frequency, duration, sampling_rate, vol)
+            bcarrier = carrier.tobytes()
+        
+            modulated_signal = np.append(modulated_signal, bcarrier)
+        modulated_signals.append(modulated_signal)
 
-    for i, bit in enumerate(binary_data):
-        frequency = mark_frequency if bit == 1 else space_frequency
-        carrier = generate_carrier(frequency, duration, sampling_rate, vol)
-        bcarrier = carrier.tobytes()
-        for i in range(len(bcarrier)):
-            modulated_signal.append(bcarrier[i])
-
-    return modulated_signal
+    return modulated_signals
 
 def make_parity(packet_data):
     res = 0
-    for i in range(len(packet_data)):
-        res ^= packet_data[i]
+    for bit in packet_data:
+        if bit == 1:
+            res += 1
     
-    return res
+    if res % 2 == 0:
+        return 0
+    else:
+        return 1
 
 def make_packet(bin_data, bodySize=16):
     packets = []
@@ -44,8 +50,7 @@ def make_packet(bin_data, bodySize=16):
                 packet.append(bin_data[i+j])
             except IndexError:
                 packet.append(0)
-
-        packet.append(make_parity(packet[i:i+4+bodySize]))
+        packet.append(make_parity(packet[0:bodySize+5]))
         packets.append(list(packet))
     return packets
 
@@ -81,7 +86,7 @@ def test_resonance():
 paudio = pyaudio.PyAudio()
 vol = 0.5
 sr = 48000
-duration = 2
+duration = 0.625
 mark_frequency = 20150  # 1
 space_frequency = 20400 # 0
 
@@ -89,18 +94,23 @@ space_frequency = 20400 # 0
 with open('testfile', 'rb') as file:
     content = file.read()
 binary_data = np.unpackbits(np.frombuffer(content, dtype=np.uint8))
-
 #binary_data = [0,0,1,0,1,1,0,0,1,1,0,0,1,1,0,0]
+#binary_data = [1, 1,1,1,1,1,1,1,1,1]
 binary_data = list(make_packet(binary_data, 8))
 #test_resonance() 
 
 # Uncomment to play the sound
 print(binary_data)
-for packet in binary_data:  
-    play_sound(packet, mark_frequency, space_frequency, sr, duration, vol)
-    time.sleep(2)
-# modulated_signal = modulate_data(binary_data, mark_frequency, space_frequency, sr, duration, vol)
-# ostream = paudio.open(format=pyaudio.paFloat32, channels=1, rate=sr, output=True)
-# ostream.write(bytes(modulated_signal))
-# ostream.close()
-# paudio.terminate()
+# for packet in binary_data:  
+#     play_sound(packet, mark_frequency, space_frequency, sr, duration, vol)
+#     time.sleep(2)
+#binary_data = [[1,0,1,0,1,1,1,1]]
+modulated_signals = modulate_data(binary_data, mark_frequency, space_frequency, sr, duration, vol)
+#print(modulated_signal.tobytes())
+for modulated_signal in modulated_signals:
+    time.sleep(1.5)
+    ostream = paudio.open(format=pyaudio.paFloat32, channels=1, rate=sr, output=True)
+    ostream.write(modulated_signal.tobytes())
+    ostream.close()
+    
+paudio.terminate()
